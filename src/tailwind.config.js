174a938d51ea178a0cd1a2ee969a8d09d2d78450
Izabela K <aizeilla12344@gmail.com>
2026-07-17 23:39:03 +0200

    Tailwind typography and daisy ui config

---
+++ src/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter var", "ui-sans-serif", "system-ui"],
+      serif: ["Inter var", "ui-serif", "system-ui"],
+    },
+    fontSize: {
+      sm: "0.675rem",
+      base: "1.3rem",
+      xl: "1.55rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+  },
+  daisyui:{
+    themes: [{
+        'fantasy' : {
+            primary: '#0000ff',
+            'primary-content': 'white',
+            secondary: '#F6F6F6',
+            neutral: '#E8E8E8'
+            },
+        },
+    ],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: '',
+    logs: true,
+    themeRoot: ':root'
+
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@daisyui")
+  ]
+}
