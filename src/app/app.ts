e71b0e157e1f4470157eefb147a232a93cc3fb54
Izabela K <aizeilla12344@gmail.com>
2026-07-19 01:49:04 +0200

    Added font awesome library to project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,30 @@
-import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaConfig, FaIconLibrary, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('task-frontend');
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void{
+    this.initFontAwesome();
+  }
+
+/**
+ * Initializes FontAwesome icons and sets default prefix.
+ * Summary: Registers icons globally for the app.
+ */
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,13 @@
+/**
+ * <summary>
+ * Central registry of FontAwesome icons used across the application.
+ * Add icons here to make them available globally through FaIconLibrary.
+ * </summary>
+ */
+
+import { IconDefinition } from '@fortawesome/angular-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser
+]
